888701ac2149f0f834714ed59c08016e13b44040
dwhiteoak <d.whiteoak@ncl.ac.uk>
2017-04-20 10:47:30 +0100

    Update headerDetails.user.js

--- headerDetails.user.js
+++ headerDetails.user.js
@@ -1,14 +1,3 @@
-    /*default values for new incident creation */
-    var raisedUser              = "nib8";
-    var raisedUserFull          = "Ian Bettison";
-    var location                = "Clinical Platforms";
-    var impact                  = "Low";
-    var urgency                 = "Low";
-    var responseLevel           = "Priority 4 - Low";
-    var suggestedGroup          = "FMS IT SUPPORT";
-    var ticketSource            = "Email";
-    var linkToLanSweeper        = "http://crf-psrv:81/user.aspx?username=<<userName>>&userdomain=CAMPUS";
-    /*variables for the signOff signature*/
     var FMSLocation             = "Clinical Research Platforms";
     var FMSTelephone            = "T: 0191 20 84670";
     var FMSEmail                = "E: david.whiteoak@ncl.ac.uk";
