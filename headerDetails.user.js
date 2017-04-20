28008bc884f329a3d99474467b8599c1cc09b758
dwhiteoak <d.whiteoak@ncl.ac.uk>
2017-04-20 10:46:47 +0100

    Changes to contact details

--- headerDetails.user.js
+++ headerDetails.user.js
@@ -10,8 +10,8 @@
     var linkToLanSweeper        = "http://crf-psrv:81/user.aspx?username=<<userName>>&userdomain=CAMPUS";
     /*variables for the signOff signature*/
     var FMSLocation             = "Clinical Research Platforms";
-    var FMSTelephone            = "T: 0191 20 84650";
-    var FMSEmail                = "E: ian.bettison@ncl.ac.uk";
+    var FMSTelephone            = "T: 0191 20 84670";
+    var FMSEmail                = "E: david.whiteoak@ncl.ac.uk";
     var signOff                 = "\n\nThanks for your attention,\n\n"+raisedUserFull+"\n"+FMSLocation+"\n"+FMSTelephone+"\n"+FMSEmail+"\n";
 
     /*setup the default responses to your canned answers here*/
